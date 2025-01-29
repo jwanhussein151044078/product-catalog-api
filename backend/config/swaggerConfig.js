925af7a98b1d6c2f52364710caa725c7135656bf
jwan hussein <jwan.hussein.1993@gmail.com>
2025-01-29 03:33:34 +0300

    Update swaggerConfig.js

--- backend/config/swaggerConfig.js
+++ backend/config/swaggerConfig.js
@@ -4,7 +4,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Backend Developer Test Project',
+      title: 'Test Project',
       version: '1.0.0',
       description: 'API documentation for the catalog service project',
     },
